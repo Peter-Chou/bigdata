4a8e5185875ab78dc5f86b32a736bec4a0000c4d
Peter-Chou <zhou_00009@126.com>
2021-05-30 16:00:27 +0800

    complete wordCount

--- src/main/scala/peter/playground/wordcount/WordCount.scala
+++ src/main/scala/peter/playground/wordcount/WordCount.scala
@@ -2,11 +2,20 @@ package peter.playground.wordcount
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    val config: SparkConf =
+    val conf: SparkConf =
       new SparkConf().setMaster("local[*]").setAppName("WordCount")
-    val sc: SparkContext = new SparkContext(config)
+    val sc: SparkContext = new SparkContext(conf)
+
+    val lines: RDD[String] = sc.textFile("data/wordcount")
+    val words: RDD[String] = lines.flatMap(_.split(" "))
+
+    val wordsTuple: RDD[(String, Int)] = words.map((_, 1))
+    val wordsCount: RDD[(String, Int)] = wordsTuple.reduceByKey(_ + _)
+    wordsCount.collect().foreach(println)
+    sc.stop()
   }
 }
