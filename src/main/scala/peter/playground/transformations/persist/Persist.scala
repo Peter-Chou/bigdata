b508956df72d705d0a1fb22c4a7d89bf60ff51be
Peter-Chou <zhou_00009@126.com>
2021-06-08 15:01:23 +0800

    :lipstick:

--- src/main/scala/peter/playground/transformations/persist/Persist.scala
+++ src/main/scala/peter/playground/transformations/persist/Persist.scala
@@ -16,8 +16,8 @@ object Persist {
 
     val wordsTuple: RDD[(String, Int)] = words.map((_, 1))
 
-    // cache: 将数据临时存储在内存中，以便之后数据重用
-    // persist: 将数据临时存储在磁盘/内存中进行数据重用,性能较低，如果作业执行完毕,临时保存的数据会丢失
+    // cache: 将数据临时存储在内存中，以便之后数据重用, 会在血缘关系中添加新的依赖。
+    // persist: 将数据临时存储在磁盘/内存中进行数据重用,性能较低，如果作业执行完毕,临时保存的数据会丢失，会在血缘关系中添加新的依赖。
     // checkpoint: 将数据长久的保存在磁盘,一般情况下会独立执行作业,为提高效率，一般和cache配合使用
     wordsTuple.cache()
     wordsTuple.checkpoint()
