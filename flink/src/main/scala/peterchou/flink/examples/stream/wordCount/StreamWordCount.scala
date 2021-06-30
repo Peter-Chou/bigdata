96c35df0c948414bc8efcadf7ea6bb802d698cfe
Peter-Chou <zhou_00009@126.com>
2021-06-30 14:58:19 +0800

    :lipstick:

--- flink/src/main/scala/peterchou/flink/examples/stream/wordCount/StreamWordCount.scala
+++ flink/src/main/scala/peterchou/flink/examples/stream/wordCount/StreamWordCount.scala
@@ -8,6 +8,7 @@ object StreamWordCount {
     // 创建流式环境
     val env: StreamExecutionEnvironment =
       StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(4)
 
     // 接受一个socket 文本流
     val inputDataStream: DataStream[String] =
@@ -18,10 +19,13 @@ object StreamWordCount {
       .flatMap(_.split(" "))
       .filter(_.nonEmpty)
       .map((_, 1))
+      // flink 每个算子相互独立，可以独立设置每个算子的并行度
+      .setParallelism(3)
       .keyBy(0)
       .sum(1)
+      .setParallelism(2)
 
-    resultDataStream.print()
+    resultDataStream.print().setParallelism(1)
 
     // 启动任务执行
     env.execute("stream word count")
